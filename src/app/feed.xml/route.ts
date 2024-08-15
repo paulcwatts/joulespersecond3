43c87af84d5fbec1c42730c4b6900a36859348b2
Paul Watts <paulcwatts@gmail.com>
2024-08-15 11:04:20 -0700

    Add a default for the NEXT_PUBLIC_SITE_URL
    For some reason Vercel doesn't want to include the environment variable in the build step.

--- src/app/feed.xml/route.ts
+++ src/app/feed.xml/route.ts
@@ -3,11 +3,7 @@ import * as cheerio from 'cheerio'
 import { Feed } from 'feed'
 
 export async function GET(req: Request) {
-  let siteUrl = process.env.NEXT_PUBLIC_SITE_URL
-
-  if (!siteUrl) {
-    throw Error('Missing NEXT_PUBLIC_SITE_URL environment variable')
-  }
+  let siteUrl = process.env.NEXT_PUBLIC_SITE_URL ?? "https://joulespersecond.com"
 
   let author = {
     name: 'Paul Watts',
